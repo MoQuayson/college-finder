9e4d6b5dc73d815dbb3d965a1488bd0062093ca7
Moses Quayson <mosesquayson1997@gmail.com>
2022-05-22 22:29:35 +0000

    code changes and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,97 @@
 import React, { Component } from 'react';
-import { Button, Card, Col, Container, FormControl, InputGroup, Row, Stack, Table } from 'react-bootstrap';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './../src/app.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import ReactPaginate from "react-paginate";
+import { Button, Card, Col, Container, Form, FormControl, InputGroup, Navbar, Row, Table } from 'react-bootstrap';
+import Pagination from 'react-js-pagination';
+import './app.css';
 
 class App extends Component{
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state = {
-            error: null,
+        this.state={
             isloaded:false,
+            error:null,
             data:[],
             activePage: 1,
-            itemsPerPage: 0,
+            itemsCountPerPage: 15,
             totalItemsCount: 0,
             pageRangeDisplayed: 0,
-            value : 'Ghana',
-            numberOfRecordsVisited:0
-        };
+            isDataFetched:false,
+            search:'',
+            isSearchBtnClicked:false,
+            totalItemsCount:0
+        }
 
-        this.TextValueChanged = this.TextValueChanged.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
     }
+
 
     FormSubmit=(e)=>{
         e.preventDefault();
+        this.handleSearchBtnClick();
     }
 
-    TableHeader(){
+    handlePageChange(pageNumber) {
+        this.setState({ activePage: pageNumber });
+    }
+
+    handleSearchBtnClick(){
+        //document.getElementById('app-card').classList.add('active');
+        this.setState({
+            isSearchBtnClicked:true,
+            isloaded:false,
+            isDataFetched:true,
+            totalItemsCount:0,
+        })
+    }
+
+    //NavBar Header
+    AppNavNar = ()=>{
         return(
-            <thead className='table-dark'>
+            <Navbar id='app-navbar' expand='lg' bg='white' variant='light' className='shadow-sm' sticky='top'>
+                <Container>
+                    <Navbar.Brand >UniSearch</Navbar.Brand>
+                </Container>
+            </Navbar>
+        )
+    }
+
+    AppSearchPanel=()=>{
+        return(
+            <div id='app-search-panel' className="d-block w-100 text-center mb-3">
+                <h4 className='app-title'>UniSearch</h4>
+                <p className='app-desc text-black-50'>An online university search engine</p>
+                <Row>
+                    <Col>
+                        <Form onSubmit={this.FormSubmit.bind(this)}>
+                            <InputGroup className='shadow-sm'>
+                                <FormControl type='text' size='lg' placeholder='Search by country' onInput={(e)=>this.setState({search:e.target.value})}/>
+                                <Button variant="success" id="searchbtn" size='lg' onClick={this.handleSearchBtnClick.bind(this)}>
+                                <i className="bi bi-search me-2"></i>Search</Button>
+                            </InputGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+    TableHeader=()=>{
+        return(
+            <thead>
             <tr >
-                <th scope="col">#</th>
-                <th scope="col">Website</th>
+                <th scope="col" hidden>#</th>
                 <th scope="col">Name</th>
-                <th scope="col">Country</th>
-                <th scope="col">Domains</th>                            
+                <th scope="col">Website</th>
+                <th scope="col">Domains</th>  
+                <th scope="col">Country</th>                          
             </tr>
             </thead>
         );
+
     }
 
     TableDetails(props){
-        const data = props.data;
-        const isloaded = props.isloaded;
+        const {data,isloaded} = props;
+
         if(isloaded === false){
             return (
                 <tr>
@@ -56,8 +103,7 @@ class App extends Component{
                 </tr>
             );
         }
-        else{
-            if(data.length === 0){
+        else if(data.length === 0){
                 return(
                     <tr>
                     <td className="text-center" colSpan="12" style={{color:'red'}}>
@@ -65,54 +111,79 @@ class App extends Component{
                     </td>
                     </tr> 
                 );
-            }
+        }
             else{
                 return(
                     data.map((item,index)=>(
                         <tr key={index}>
-                            <td>{index+1}</td>
+                            <td hidden>{index+1}</td>
+                            <td className='fw-bold'>{item.name}</td>
                             <td><a target='_blank' href={item.web_pages}>{item.web_pages}</a></td>
-                            <td><strong>{item.name}</strong></td>
-                            <td>{item.country}</td>
                             <td>{item.domains}</td>
+                            <td>{item.country}</td>
                         </tr>
                     ))
-
-                    /*data.slice(
-                        this.state.numberOfRecordsVisited,
-                        this.state.numberOfRecordsVisited + this.state.itemsPerPage
-                    )
-                    .map((item,index)=>(
-                        <tr key={index}>
-                            <td>{index+1}</td>
-                            <td><a target='_blank' href={item.web_pages}>{item.web_pages}</a></td>
-                            <td><strong>{item.name}</strong></td>
-                            <td>{item.country}</td>
-                            <td>{item.domains}</td>
-                        </tr>
-                    ))*/
                 );
             }
+        
+        
+    }
+
+
+    AppSearchResults=(props)=>{
+        const{isDataFetched,data,isloaded} = props;
+        if(isDataFetched)
+        {
+            return(
+                <Card id='app-card' className='shadow-sm border-0'>
+                    <Card.Header>
+                        <h5>Universities ({this.state.totalItemsCount})</h5>
+                    </Card.Header>            
+                    <Card.Body className='p-1'>
+                        <div className='table-responsive-lg'>
+                            <Table responsive='lg' hover striped>
+                                <this.TableHeader/>
+                                <tbody>
+                                <this.TableDetails data={data} isloaded={isloaded}/>
+                                </tbody>
+                            </Table>
+                        </div>
+                    </Card.Body>
+
+                    <Card.Footer className='d-flex justify-content-end align-items-end bg-white pt-1'>
+                        <Pagination
+                            activePage={this.state.activePage}
+                            itemsCountPerPage={this.state.itemsCountPerPage}
+                            totalItemsCount={this.state.totalItemsCount}
+                            pageRangeDisplayed={5}
+                            onChange={this.handlePageChange}
+                            itemClass='page-item'
+                            linkClass='page-link'
+                        />
+                    </Card.Footer>
+                </Card>
+            )
+        }
+        else{
+            return null;
         }
         
     }
 
-    DataFetch(){
-        //console.log(this.state.value);
-        var url = 'http://universities.hipolabs.com/search?country='+ this.state.value;
+    handleUniDataFetch(){
+        var url = 'http://universities.hipolabs.com/search?country='+ this.state.search;
         fetch(url)
         .then(res=>res.json())
         .then(
-            (result) =>{
+            (response) =>{
                 this.setState({
                     isloaded: true,
-                    data: result,
-                    totalItemsCount: result.length,
-                    itemsPerPage: 5,
-                    pageRangeDisplayed: Math.ceil(result.length / 5),
-                    numberOfRecordsVisited:this.state.activePage * 5
+                    data: response,
+                    pageRangeDisplayed:5,
+                    totalItemsCount:response.length,
+                    isSearchBtnClicked:false,
+                    totalItemsCount:response.length
                 });
-                console.log(this.state.numberOfRecordsVisited);
             },
             (error)=>{
                 this.setState({
@@ -123,76 +194,28 @@ class App extends Component{
                 //console.log(this.state.error);
             }
         )
+
     }
 
-    changePage = ({ selected }) => {
-        //setPage(selected);
-        this.setState({activePage:selected});
-      };
-
-     ButtonClicked=()=>{
-        this.DataFetch();
-        <this.TableDetails items={this.state.data} isloaded={this.state.isloaded} error={this.state.error}/>
+    componentDidUpdate(){
+        if(this.state.isSearchBtnClicked){
+            this.handleUniDataFetch();
+        }
     }
 
-    TextValueChanged=(e)=> {
-        var value = e.target.value;
-        this.setState({value: value});
-    }
-
-    componentDidMount(){
-        this.DataFetch();
-    }
     render(){
-        const { data, error, isloaded, value} = this.state;
+        const{data,isloaded,isDataFetched,error,activePage,itemsCountPerPage,totalItemsCount} = this.state;
+        let items = data.slice((activePage - 1) * itemsCountPerPage, activePage * itemsCountPerPage);
         return(
-            <Container className='p-5 mt-5'>
-                <Stack gap='1'>
-                <h2 className='text-center mb-4'>Welcome to University Information Portal</h2>
-                <Row>
-                    <Col className="mb-3">
-                    <form onSubmit={this.FormSubmit.bind(this)}>
-                        <div className="input-group" hidden>
-                            <input type="text" className="form-control form-control-lg" id="country" name="country" 
-                            placeholder="Enter Country Name" aria-describedby="searchbtn" onChange={this.TextValueChanged} required/>
-
-                            <button className="btn btn-success" id="searchbtn" onClick={this.ButtonClicked}>Search Country</button>
-                        </div>
-
-                        <InputGroup className='shadow'>
-                            <FormControl type='text' size='lg' placeholder='Search by country name' onChange={this.TextValueChanged}/>
-                            <Button variant="primary" id="searchbtn" onClick={this.ButtonClicked.bind(this)}>
-                            <i className="bi bi-search me-2"></i>Search</Button>
-                        </InputGroup>
-                    </form>
-                    </Col>
-                </Row>
-
-                <Card className='shadow'>
-                    <Card.Body className='p-0'>
-                    <Table responsive='lg' hover>
-                        <this.TableHeader/>
-                        <tbody>
-                        <this.TableDetails isloaded={isloaded} data={data}/>
-                        </tbody>
-                    </Table>
-                    </Card.Body>
-
-                    <Card.Footer>
-                    <ReactPaginate
-                        previousLabel={"Previous"}
-                        nextLabel={"Next"}
-                        
-                        containerClassName={"navigationButtons"}
-                        previousLinkClassName={"previousButton"}
-                        nextLinkClassName={"nextButton"}
-                        disabledClassName={"navigationDisabled"}
-                        activeClassName={"navigationActive"}
-                        />
-                    </Card.Footer>
-                </Card>
-                </Stack>
-            </Container>
+            <>
+                <this.AppNavNar/>
+                <div id="main">
+                    <Container>
+                        <this.AppSearchPanel/>
+                        <this.AppSearchResults isDataFetched={isDataFetched} data={items} isloaded={isloaded}/>
+                    </Container>
+                </div>
+            </>
         )
     }
 }
