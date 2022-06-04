eb17132c37a3ccd4d3ee6d6f5ad50bf69e7a3bad
Moses Quayson <mosesquayson1997@gmail.com>
2022-06-04 15:56:12 +0000

    code changes and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ class App extends Component{
                 <p className='app-desc text-black-50'>An online university search engine</p>
                 <Row>
                     <Col>
-                        <Form onSubmit={this.FormSubmit.bind(this)}>
+                        <Form >
                             <InputGroup className='shadow-sm'>
                                 <FormControl type='text' size='lg' placeholder='Search by country' onInput={(e)=>this.setState({search:e.target.value})}/>
                                 <Button variant="success" id="searchbtn" size='lg' onClick={this.handleSearchBtnClick}>
