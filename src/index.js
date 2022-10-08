bf988cdbcdf1a2274f9ef1a68c9616f4459fb93f
Moses Quayson <mosesquayson1997@gmail.com>
2022-10-08 16:29:28 +0000

    code changes and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import './app.css';
+import './main.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
